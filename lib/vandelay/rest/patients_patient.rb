30302cb04d90a7622a74892e3b5e51ce1b65b73b
Partho Ghosh <partho.ghosh24@gmail.com>
2024-05-29 21:08:24 -0600

    When changes applied, this will add an endpoint for single patient.

--- lib/vandelay/rest/patients_patient.rb
+++ lib/vandelay/rest/patients_patient.rb
@@ -4,8 +4,20 @@ require 'vandelay/services/patient_records'
 module Vandelay
   module REST
     module PatientsPatient
+      def self.patients_srvc
+        @patients_srvc ||= Vandelay::Services::Patients.new
+      end
+
       def self.registered(app)
-        # add endpoint code here
+        app.get '/patients/:id' do
+          id = Integer(params[:id]) rescue nil
+          halt(400, {message: 'Invalid id'}.to_json) unless id
+
+          result = Vandelay::REST::PatientsPatient.patients_srvc.retrieve_one(id)
+          halt(404, {message: 'Patient not found'}.to_json) unless result
+          
+          json(result.to_h)
+        end
       end
     end
   end
